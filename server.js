a0ca579be66e3b9bef5dd16d473163e54e8c4fca
Utkarsh Gupta <ut.gupta29@gmail.com>
2024-01-29 01:59:54 +0530

    mongo connect

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express=require('express')
+const mongoose = require('mongoose')
 const app=express()
 
 app.get('/',function(req,res){
-    res.send('Hello Node')
+    res.send('Hello Node api hey g')
 })
-app.listen(3000)
+
+mongoose.connect('mongodb+srv://utkarsh:Utkarsh123@utkarshapi.06cyvww.mongodb.net/Node-API')
+.then(()=>{
+    console.log("hello Mongo")
+    app.listen(3000)
+}).catch((error)=>{
+    console.log(error)
+})
